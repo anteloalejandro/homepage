7e9b321b55f58bd89486c4d560dd3dd0dce7a196
Alejandro Antelo <alfasant@protonmail.com>
2024-11-02 03:35:43 +0100

    misc. styling fixes

--- code.js
+++ code.js
@@ -1,3 +1,4 @@
+const wrapper = document.querySelector('.wrapper');
 /** @type HTMLElement[] */
 const actionables = [...document.querySelectorAll('[data-action-key]')];
 
@@ -27,6 +28,7 @@ document.addEventListener('keydown', ev => {
   if (groupKeyPressed) {
     const group = actionGroups.find(el => el.parent.dataset.actionGroup == groupKeyPressed);
     group.parent.classList.remove('show-actions');
+    wrapper.classList.remove('hide-non-group-actions');
     const element = group.actionables.find(el => el.dataset.actionGroupKey == ev.key);
     if (element) {
       ev.preventDefault();
@@ -37,6 +39,7 @@ document.addEventListener('keydown', ev => {
     groupKeyPressed = null;
   } else if (aux = actionGroups.find(el => el.parent.dataset.actionGroup == ev.key)) {
     aux.parent.classList.add('show-actions')
+    wrapper.classList.add('hide-non-group-actions');
     groupKeyPressed = ev.key;
     // console.log(`group ${ev.key}. waiting for group action`);
   } else {

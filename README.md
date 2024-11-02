17fb055463bd5a9770c46bc6bb31259246ed0157
Alejandro Antelo <alfasant@protonmail.com>
2024-11-02 01:22:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TO-DO
+- Teclas asignadas a acciones:
+  / para buscar, asdf para accionar los acordeones, qwerty para los enlaces rápidos...
+- Emblemas en las cosas accionables con la tecla que las acciona
+- Teclas configurables a traves de data attributes en el HTML
+- Expandir ventana bookmarks (html dialog?) y añadir búsqueda (no importante)
+- Elegir imágen al azar de entre colección
+- Conservar estado a través de localStorage
+- fuente del sistema
+- **Minimizar la complejidad del html, no build, puro js+html+css**
---
+++ code.js
@@ -0,0 +1,54 @@
+/** @type HTMLElement[] */
+const actionables = [...document.querySelectorAll('[data-action-key]')];
+
+/** @typedef
+ *  @prop {HTMLElement} parent
+ *  @prop {HTMLElement[]} actionables
+ */
+const actionGroups =
+  [...document.querySelectorAll('[data-action-group]')]
+    .map(el => {
+      return {
+        parent: el,
+        actionables: [...el.querySelectorAll('[data-action-group-key]')]
+      }
+    })
+
+/** @type string*/
+groupKeyPressed = null;
+
+document.querySelectorAll('input').forEach(input => {
+  input.addEventListener('keydown', ev => {
+    ev.stopPropagation();
+  })
+})
+document.addEventListener('keydown', ev => {
+  let aux;
+  if (groupKeyPressed) {
+    const group = actionGroups.find(el => el.parent.dataset.actionGroup == groupKeyPressed);
+    group.parent.classList.remove('show-actions');
+    const element = group.actionables.find(el => el.dataset.actionGroupKey == ev.key);
+    if (element) {
+      ev.preventDefault()
+      element.focus();
+      element.click();
+      console.log(`action ${ev.key}`);
+    }
+    // element && console.log(`group action ${ev.key}`);
+    groupKeyPressed = null;
+  } else if (aux = actionGroups.find(el => el.parent.dataset.actionGroup == ev.key)) {
+    aux.parent.classList.add('show-actions')
+    groupKeyPressed = ev.key;
+    // console.log(`group ${ev.key}. waiting for group action`);
+  } else {
+    groupKeyPressed = null;
+    const element = actionables.find(el => el.dataset.actionKey == ev.key);
+    if (element) {
+      ev.preventDefault()
+      element.focus();
+      element.click();
+      console.log(`action ${ev.key}`);
+    }
+    // console.log(`action ${ev.key}`);
+  }
+});
